84473c05bb9d97eda5f52f1c09f717e0107585a3
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-08-31 18:00:36 +0600

    added combaining array

---
+++ combainingArray.js
@@ -0,0 +1,16 @@
+/**
+ * 5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+
+Use the concat method to combine the two arrays into a new array.
+
+Print both the original arrays and the combined array using console.log().
+ */
+
+
+const numbers = [23,5,7,90,1,33];
+const names = ["abid","korim","rohim","sazzad"];
+
+console.log(numbers.concat(names));
