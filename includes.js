9db6d349bcd113eaf97d5e4cad884f261c153286
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-08-31 17:43:03 +0600

    added includes

---
+++ includes.js
@@ -0,0 +1,14 @@
+/**
+ * 3. Checking Array Membership with ‘includes’
+Instructions:
+
+Create an array of books containing different book.
+
+Use the includes method to check if the array contains a javascript book.
+
+Print a message to the console indicating whether the element is present in the array or not.
+ */
+
+const books = ["Bangla","English","Mathmathics","Chemistry","Physics"];
+
+console.log("There is no book whice called javascript so it's shows", books.includes("javascript"));
